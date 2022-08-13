67d88fa941fbdaabde375d9ee2bcb2442b29ca97
djimbo <djimbosfan@mail.ru>
2022-08-13 09:35:51 +0300

    Update

--- tgbot/keyboards/inline_main.py
+++ tgbot/keyboards/inline_main.py
@@ -10,13 +10,13 @@ def menu_finl(user_id):
     keyboard = InlineKeyboardBuilder(
     ).row(
         ikb("User 1", data="..."),
-        ikb("User 2", data="..."),
+        ikb("User 2", data="...")
     )
 
     if user_id in get_admins():
         keyboard.row(
             ikb("Admin 1", data="..."),
-            ikb("Admin 2", data="..."),
+            ikb("Admin 2", data="...")
         )
 
     return keyboard.as_markup()
